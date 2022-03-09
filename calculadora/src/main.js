38cd84e17f7663d870d4530f2abe65f5993be4bd
esvelpe <esneidervelpe@gmail.com>
2022-03-09 16:51:41 -0500

    Creación de la vista Home con las tarjetas

--- calculadora/src/main.js
+++ calculadora/src/main.js
@@ -3,7 +3,7 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-import router from "../node_modules/vue-router";
+import router from "./router";
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
--- calculadora/src/views/HomeView.vue
+++ calculadora/src/views/HomeView.vue
@@ -1,12 +1,75 @@
 <template>
   <main>
-    <b-card title="Figuras geométricas">
-      <b-card-text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae est
-        impedit sit quo enim laboriosam aperiam excepturi ut, autem voluptate
-        officiis optio veniam? Soluta quia corporis ullam beatae, doloremque
-        aliquid?
-      </b-card-text>
-    </b-card>
+    <b-container class="container mt-5">
+      <b-row>
+        <b-col md="6">
+          <b-card
+            style="max-width: 30rem"
+            header="Figuras geométricas"
+            header-bg-variant="info"
+            class="mb-5"
+          >
+            <b-card-text>
+              Acá podrás calcular las propiedades básicas de las figuras
+              geométricas que prefieras.
+            </b-card-text>
+            <b-button variant="primary"> Calcular </b-button>
+          </b-card>
+        </b-col>
+        <b-col md="6">
+          <b-card
+            style="max-width: 30rem"
+            header="Estadística"
+            header-bg-variant="info"
+          >
+            <b-card-text>
+              Acá podrás calcular todo lo relacionado con la estadística
+              descriptiva para un arreglo de elementos que proveas
+            </b-card-text>
+            <b-button variant="primary"> Calcular </b-button>
+          </b-card>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col md="6">
+          <b-card
+            style="max-width: 30rem"
+            header="Algebra lineal"
+            header-bg-variant="info"
+          >
+            <b-card-text>
+              Acá podrás calcular operaciones básicas del álgebra entre vectores
+              que proveas
+            </b-card-text>
+            <b-button variant="primary"> Calcular </b-button>
+          </b-card>
+        </b-col>
+        <b-col md="6">
+          <b-card
+            style="max-width: 30rem"
+            header="Ecuaciones diferenciales"
+            header-bg-variant="info"
+          >
+            <b-card-text>
+              Acá podrás seleccionar el tipo de ecuación diferencial y después
+              ingresar sus coeficientes para solucionar la ecuación diferencial
+            </b-card-text>
+            <b-button variant="primary"> Calcular </b-button>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </main>
 </template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
+
+<style scoped>
+.container {
+  margin: 10% auto;
+}
+</style>
