07f7ad96cb461db95e3491f5045d3af3b4c0a127
esvelpe <esneidervelpe@gmail.com>
2022-03-09 02:55:36 -0500

    Creación de la navbar

---
+++ calculadora/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <main id="app">
+    <px-header />
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
+  </main>
+</template>
+
+<script>
+import PxHeader from "@/components/PxHeader.vue";
+
+export default {
+  name: "App",
+  components: { PxHeader },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0;
+}
+</style>
---
+++ calculadora/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <b-card title="Figuras geométricas">
+      <b-card-text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae est
+        impedit sit quo enim laboriosam aperiam excepturi ut, autem voluptate
+        officiis optio veniam? Soluta quia corporis ullam beatae, doloremque
+        aliquid?
+      </b-card-text>
+    </b-card>
+  </main>
+</template>
